aa70877422e524d3286a82c6ec436f4f6d4dac98
vinay <vinay18011@gmail.com>
2020-01-06 01:02:33 +0530

    changes

--- AESPROJECT/src/AES/Aesalgo.java
+++ AESPROJECT/src/AES/Aesalgo.java
@@ -1,6 +1,7 @@
 package AES;
 
 import java.security.Key;
+import java.util.Base64;
 
 import javax.crypto.Cipher;
 import javax.crypto.spec.SecretKeySpec;
@@ -23,14 +24,23 @@ public class Aesalgo {
 		Key key = generateKey();
 		Cipher c = Cipher.getInstance(algo);
 		c.init(Cipher.ENCRYPT_MODE, key);
-		byte [] encryptedMsg=c.doFinal();
-		String encryptedMsg=text;
+		byte [] encMsg=c.doFinal(text.getBytes());
+		
+		System.out.println("encmsg"+encMsg);
+		String encryptedMsg = new String( Base64.getEncoder().encode(encMsg));
 		return encryptedMsg;
 	}
-	public String decrypt(String text) {
-		
-		String decryptedMsg=text;
+	public String decrypt(String text) throws Exception {
+		System.out.println("passedstring for dec "+text);
+		Key key = generateKey();
+		Cipher c = Cipher.getInstance(algo);
+		c.init(Cipher.DECRYPT_MODE, key);
+		byte [] decMsgbase64 = Base64.getDecoder().decode(text);
+		System.out.println("deccmsg 64"+decMsgbase64);
+		byte [] decMsg=c.doFinal(decMsgbase64);
+		String decryptedMsg = decMsg.toString();
 		return decryptedMsg;
+		
 	}
 
 }
--- AESPROJECT/src/AES/Main.java
+++ AESPROJECT/src/AES/Main.java
@@ -1,5 +1,21 @@
 package AES;
 
+
 public class Main {
 
+	
+	
+	
+	public static void main(String args[]) throws Exception {
+		Aesalgo aalgo = new Aesalgo("vinayvinayvinayq");
+		String s = "my name is vinay";
+		System.out.println("Original text is "+s );
+		String encryptedtext = aalgo.encrypt(s);
+		System.out.println("encrypted text is "+encryptedtext );
+		String decryptedtext = aalgo.decrypt(encryptedtext);
+		
+		System.out.println("decryptedtext text is "+decryptedtext );
+		
+		
+	}
 }
